ce54a3df17de901f5f99686cfa772710119c95a9
DESKTOP-V8UD4Q9\Onini <butsakorn.buts@bumail.net>
2021-01-29 22:11:42 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/TextMaching/Searching.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Searching : MonoBehaviour
+{
+    public Text Show_text;
+    string Friend;
+    public Text input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Work()
+    {
+        Friend = input.text;
+        if (Friend == "Dome" || Friend == "Joe" || Friend == "Tong" || Friend == "Nea" || Friend == "Aun" || Friend == "Jean")
+        {
+            Show_text.text = Friend+" it Found";
+        }
+        else 
+        {
+            Show_text.text = Friend+" it Not Found";
+        }
+    }  
+
+}
