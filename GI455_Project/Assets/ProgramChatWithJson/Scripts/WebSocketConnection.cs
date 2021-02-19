cb63e5b1a1c9b30eb4b22bec3a829e892a388c78
dom_o <dom_o@DESKTOP-T8205DM>
2021-02-20 01:12:49 +0700

    Send Assignment Week4

---
+++ GI455_Project/Assets/ProgramChatWithJson/Scripts/WebSocketConnection.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using System;
+using UnityEngine.UI;
+
+namespace ChatWebSocketWithJson
+{
+    public class WebSocketConnection : MonoBehaviour
+    {
+        struct SocketEvent
+        {
+            public string eventName;
+            public string data;
+            public SocketEvent(string eventName, string data)
+            {
+                this.eventName = eventName;
+                this.data = data;
+            }
+        }
+        public GameObject popupCereate;
+        public GameObject popupJoin;
+        public GameObject closeMenu;
+        public GameObject rootConnection;
+        public GameObject rootMessenger;
+
+        public InputField inputUsername;
+        public InputField inputText;
+        public Text sendText;
+        public Text receiveText;
+
+        public Text C_room;
+        public Text J_room;
+
+        string roomNumber;
+        string joinNumber;
+
+        private WebSocket ws;
+
+        private string tempMessageString;
+
+        public void Start()
+        {
+            rootConnection.SetActive(true);
+            rootMessenger.SetActive(false);
+
+
+            
+        }
+
+        public void Connect()
+        {
+            string url = $"ws://127.0.0.1:36500/";
+
+            ws = new WebSocket(url);
+
+            ws.OnMessage += OnMessage;
+
+            ws.Connect();
+
+            rootConnection.SetActive(false);
+            rootMessenger.SetActive(false);
+            closeMenu.SetActive(true);
+
+
+            //CreateRoom("TestRoom01");
+        }
+        public void buttonCreate()
+        {
+            roomNumber = C_room.text;
+
+            CreateRoom(roomNumber);
+        }
+
+        public void CreateRoom(string roomName)
+        {
+            if (ws.ReadyState == WebSocketState.Open)
+            {
+                SocketEvent socketEvent = new SocketEvent("CreateRoom", roomName);
+
+                string jsonStr = JsonUtility.ToJson(socketEvent);
+
+                ws.Send(jsonStr);
+            }
+        }
+        public void buttonJoin()
+        {
+            joinNumber = J_room.text;
+
+            JoinRoom(joinNumber);
+        }
+        public void JoinRoom(string roomName)
+        {
+            if (ws.ReadyState == WebSocketState.Open)
+            {
+                SocketEvent socketEvent = new SocketEvent("JoinRoom", roomName);
+
+                string jsonStr = JsonUtility.ToJson(socketEvent);
+
+                ws.Send(jsonStr);
+                Debug.Log(jsonStr);
+            }
+        }
+
+        public void LeaveRoom(string roomName)
+        {
+            if (ws.ReadyState == WebSocketState.Open)
+            {
+                SocketEvent socketEvent = new SocketEvent("LeaveRoom", roomName);
+
+                string jsonStr = JsonUtility.ToJson(socketEvent);
+
+                ws.Send(jsonStr);
+
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (ws != null)
+                ws.Close();
+        }
+        
+        public void SendMessage()
+        {
+            if (inputText.text == "" || ws.ReadyState != WebSocketState.Open)
+                return;
+
+            ws.Send(inputText.text);
+            inputText.text = "";
+        }
+
+        private void OnDestroy()
+        {
+            if (ws != null)
+                ws.Close();
+        }
+
+        private void Update()
+        {
+            Getserver();
+        }
+
+        public void Getserver()
+        {
+            if (tempMessageString != null && tempMessageString != "")
+            {
+                SocketEvent getserverData = JsonUtility.FromJson<SocketEvent>(tempMessageString);
+                Debug.Log(getserverData.eventName);
+                Debug.Log(getserverData.data);
+                if (getserverData.eventName == "CreateRoom")
+                {
+
+                    if (getserverData.data == "Success")
+                    {
+
+                        rootConnection.SetActive(false);
+                        rootMessenger.SetActive(true);
+                        closeMenu.SetActive(false);
+
+                    }
+                    else if (getserverData.data == "fail")
+                    {
+                        popupCereate.SetActive(true);
+                    }
+
+                }
+                else if(getserverData.eventName == "JoinRoom")
+                {
+                    if (getserverData.data == "Success")
+                    {
+                        rootConnection.SetActive(false);
+                        rootMessenger.SetActive(true);
+                        closeMenu.SetActive(false);
+                    }
+                    else if (getserverData.data == "fail")
+                    {
+                        popupJoin.SetActive(true);
+                    }
+                }
+                else if (getserverData.eventName == "LeaveRoom")
+                {
+                    rootConnection.SetActive(false);
+                    rootMessenger.SetActive(false);
+                    closeMenu.SetActive(true);
+                }
+                tempMessageString = "";
+
+            }
+        }
+
+        private void OnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            Debug.Log(messageEventArgs.Data);
+            tempMessageString = messageEventArgs.Data;
+        }
+
+        
+    }
+
+}
+
+
--- NodeJS/server.js
+++ NodeJS/server.js
@@ -5,23 +5,217 @@ var websocketServer = new websocket.Server({port:36500},()=> {
 });
 
 var wsList = [];
+var roomList = [];
 
 websocketServer.on("connection" , (ws, rq)=>{
+    {
+        //LobbyZone
+        ws.on("message" , (data)=>{
+            
+            console.log(data);
 
-    console.log('Connected.');
+            var toJson = JSON.parse(data);
+
+            //console.log(toJson.eventName);
+
+            if(toJson.eventName == "CreateRoom")//CreateRoom
+            {
+                console.log("client request CreateRoom ["+toJson.data+"]");
+                var isFoundRoom = false;
+                for (let i = 0; i < roomList.length; i++) 
+                {
+                    if (roomList[i].roomName == toJson.data)
+                    {
+                        isFoundRoom = true;
+                        break;
+                    }
+                }
+
+                if(isFoundRoom)
+                {
+                    //Callclack to client : roomName is exist.
+                    console.log("Create room : room is found");
+
+                    var resultData = {
+                        eventName: toJson.eventName,
+                        data: "fail"
+                    }
+
+                    var toJsonStr = JSON.stringify(resultData)
+
+                    ws.send(toJsonStr);
+                }
+                else
+                {
+                    //Create room here.
+                    console.log("Create room : room is not found");
+
+                    var newRoom = 
+                    {
+                        roomName: toJson.data,
+                        wsList: []
+                    }
+
+                    newRoom.wsList.push(ws);
+
+                    roomList.push(newRoom);
+
+                    var resultData = {
+                        eventName: toJson.eventName,
+                        data: "Success"
+                    }
+
+                    var toJsonStr = JSON.stringify(resultData)
+
+                    ws.send(toJsonStr);
+                }
+            }
+            else if(toJson.eventName == "JoinRoom")
+
+            {
+                console.log("client request JoinRoom ["+toJson.data+"]");
+
+                var isFoundRoom = false;
+            
+                for (let i = 0; i < roomList.length; i++) 
+                {
+                    if (roomList[i].roomName == toJson.data)
+                    {
+                        roomList[i].wsList.push(ws);
+                        isFoundRoom = true;
+                        break;
+                    }
+                
+                }
+
+                if(isFoundRoom)
+                {
+                    //Callclack to client : roomName is exist.
+                    console.log("JoinRoom : room is found");
+
+                    var resultData = {
+                        eventName: toJson.eventName,
+                        data: "Success"
+                    }
+
+                    var toJsonStr = JSON.stringify(resultData)
+
+                    ws.send(toJsonStr);
+                }
+                else
+                {
+                    //Create room here.
+                    console.log("JoinRoom : room is not found");
+
+
+                    var resultData = {
+                        eventName: toJson.eventName,
+                        data: "fail"
+                    }
+
+                    var toJsonStr = JSON.stringify(resultData)
+
+                    ws.send(toJsonStr);
+                }
+            }
+            else if(toJson.eventName == "LeaveRoom")
+            {
+                    var isLeaveSuccess = false;
+                for(var i = 0; i < roomList.length; i++)
+                {
+                    for(var j = 0; j < roomList[i].wsList.length; j++)
+                    {   
+                        if(ws == roomList[i].wsList[j])
+                        {
+                            roomList[i].wsList.splice(j, 1);
+
+                            if(roomList[i].wsList.length <= 0)
+                            {
+                                roomList.splice(i, 1);
+                            }
+                            isLeaveSuccess = true;
+                            break;
+                        }
+                    }
+                }
+
+                if(isLeaveSuccess)
+                {
+                    var resultData = {
+                        eventName:"LeaveRoom",
+                        data:"Success"
+                    }
+                    var toJsonStr = JSON.stringify(resultData);
+                    ws.send(toJsonStr);
+
+                console.log("leave room Success");
+                }
+                else
+                {
+                    var resultData = {
+                        eventName:"LeaveRoom",
+                        data:"fail"
+                    }
+                    var toJsonStr = JSON.stringify(resultData);
+                    ws.send(toJsonStr);
+
+                    console.log("leave room fail");
+                }
+            }
+        });     
+    }
+    console.log('Client Connected.');
     
     wsList.push(ws);
-
-    ws.on("message" , (data)=>{
-
-        console.log("send from cilent : "+data);
-        Boardcast(data);
-    });
 
     ws.on("close", ()=>{
 
         wsList = ArrayRemove(wsList, ws);
         console.log("disconnected.");
+
+        var isLeaveSuccess = false;
+                for(var i = 0; i < roomList.length; i++)
+                {
+                    for(var j = 0; j < roomList[i].wsList.length; j++)
+                    {   
+                        if(ws == roomList[i].wsList[j])
+                        {
+                            roomList[i].wsList.splice(j, 1);
+
+                            if(roomList[i].wsList.length <= 0)
+                            {
+                                roomList.splice(i, 1);
+                            }
+                            isLeaveSuccess = true;
+                            break;
+                        }
+                    }
+                }
+
+                if(isLeaveSuccess)
+                {
+
+                    var resultData = {
+                        eventName:"LeaveRoom",
+                        data:"Success"
+                    }
+                    var toJsonStr = JSON.stringify(resultData);
+                    ws.send(toJsonStr);
+
+                console.log("leave room success");
+                }
+                else
+                {
+
+                    var resultData = {
+                        eventName:"LeaveRoom",
+                        data:"fail"
+                    }
+                    var toJsonStr = JSON.stringify(resultData);
+                    ws.send(toJsonStr);
+
+                    console.log("leave room fail");
+                }
     });
 });
 
